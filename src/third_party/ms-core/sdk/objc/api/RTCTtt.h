f90265a8ab49336e3003d3d686268ff6d32b5280
hao.yu <hao.yu@ushow.media>
2022-04-13 17:54:49 +0800

    解决mac版本链接失败问题(Undefined symbols for architecture x86_64:_OBJC_CLASS_)

---
+++ src/third_party/ms-core/sdk/objc/api/RTCTtt.h
@@ -0,0 +1,17 @@
+//
+//  ttt.h
+//  test007
+//
+//  Created by yuhao on 2022/4/13.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RTCTtt : NSObject
+
+-(void)t;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ src/third_party/ms-core/sdk/objc/api/RTCTtt.mm
@@ -0,0 +1,16 @@
+//
+//  ttt.m
+//  test007
+//
+//  Created by yuhao on 2022/4/13.
+//
+
+#import "RTCTtt.h"
+
+@implementation RTCTtt
+
+
+-(void)t {
+    // NSLog(@"123!");
+}
+@end
